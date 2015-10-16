708e6773a2907101da5af3b4df139406155f6181
Oscar Peña <oscar.pdr@gmail.com>
2015-10-16 15:40:03 +0200

    Massive name refactoring

--- lunchman/manage.py
+++ lunchman/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunchtime_organiser.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lunchman.settings")
 
     from django.core.management import execute_from_command_line
 
